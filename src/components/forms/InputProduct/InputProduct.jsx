5d3b4f373d4de509ad84db1bcf7d5eb8f24ce11a
MauricioZapata00 <maurozac123@gmail.com>
2023-05-13 01:09:17 -0500

    Adding the convertion from byteArray to image, this is kind of tricky but then we will have a S3 bucket to simply store URL images

--- src/components/forms/InputProduct/InputProduct.jsx
+++ src/components/forms/InputProduct/InputProduct.jsx
@@ -1,7 +1,11 @@
-import { Box, Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, TextField } from '@mui/material'
+import { Box, Button, Dialog, DialogActions, DialogContent, DialogTitle, TextField } from '@mui/material'
 import React, { useState } from 'react'
 
 const InputProduct = (props) => {
+
+    const imageContainerId = 'image-container-id'
+
+    const imageCreatedId = 'image-created-id'
 
     const { isOpenDialog, setIsOpenDialog } = props
 
@@ -9,7 +13,9 @@ const InputProduct = (props) => {
 
     const [productDescription, setProductDescription] = useState("")
 
-    const [productImage, setProductImage] = useState("")
+    const [productImageURL, setproductImageURL] = useState("")
+
+    const [productImageByteArray, setProductImageByteArray] = useState([])
 
     const handleProductNameChage = (event) => {
         setProductName(() => event.target.value)
@@ -19,15 +25,46 @@ const InputProduct = (props) => {
         setProductDescription(() => event.target.value)
     }
 
-    const handleProductImageChage = (event) => {
-        setProductImage(() => event.target.value)
+    const handleproductImageURLChage = (event) => {
+        const fileReceived = event.target.files[0]
+        const reader = new FileReader()
+        reader.readAsArrayBuffer(fileReceived)
+        console.log('event.target.files[0]', event.target.files[0])
+        reader.onloadend = (evt) => {
+            convertArrayBufferToImage(evt.target.result, fileReceived.type)
+        }
+        setproductImageURL(() => event.target.value)
+    }
+
+    const convertArrayBufferToImage = (arrayBuffer, type) => {
+        const byteArray = new Uint8Array(arrayBuffer)
+        setProductImageByteArray(() => [byteArray])
+        const imageBlob = new Blob([byteArray], { type: type })
+        const imageURL = URL.createObjectURL(imageBlob)
+        createDynamicPicture(imageURL)
+    }
+
+    const createDynamicPicture = (imageToCreateURL) => {
+        const imageContainer = document.getElementById(imageContainerId)
+        imageContainer.appendChild(document.createElement('img'))
+        let imageCreated = document.getElementsByTagName('img')[0]
+        imageCreated.id = imageCreatedId
+        imageCreated.src = imageToCreateURL
+    }
+
+    const createProduct = () => {
+        console.log('productImageByteArray', productImageByteArray)
     }
 
     const handleCloseDialog = () => {
         setIsOpenDialog(false)
         setProductName(() => "")
         setProductDescription(() => "")
-        setProductImage(() => "")
+        setproductImageURL(() => "")
+        const imageToRemove = document.getElementById(imageCreatedId)
+        if (imageToRemove) {
+            imageToRemove.remove()    
+        }
     }
 
 
@@ -70,14 +107,16 @@ const InputProduct = (props) => {
                         type="file"
                         id="input-product-image-id"
                         accept="image/png, image/jpeg"
-                        value={productImage}
-                        onChange={handleProductImageChage}
+                        value={productImageURL}
+                        onChange={handleproductImageURLChage}
                     />
                 </Box>
-
+                <div id={imageContainerId}>
+                    {/* <img id='image' src={productImageURL}/> */}
+                </div>
                 <DialogActions>
                     <Button onClick={handleCloseDialog}>Cancelar</Button>
-                    <Button onClick={() => { }} autoFocus>Crear producto</Button>
+                    <Button onClick={createProduct} autoFocus>Crear producto</Button>
                 </DialogActions>
             </DialogContent>
         </Dialog>
