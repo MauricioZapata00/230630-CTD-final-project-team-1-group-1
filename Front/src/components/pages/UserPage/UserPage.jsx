982c18c407959f09a0cbe26852787b13979ca7a9
Valeria Paparelli <valeria.paparelli@live.com.ar>
2023-06-02 10:52:59 -0300

    redireccion al root al desloguearse y al entrar a pagina de usuario sin usuario logueado

--- Front/src/components/pages/UserPage/UserPage.jsx
+++ Front/src/components/pages/UserPage/UserPage.jsx
@@ -1,26 +1,31 @@
 import { useContext } from "react";
 import { AppContext } from "../../../context";
+import { useNavigate } from "react-router-dom";
 
 const UserPage = () => {
-    const {userData } = useContext(AppContext);
+  const { userData } = useContext(AppContext);
+  const navigateTo = useNavigate();
 
+  if (!userData) {
+    navigateTo("/");
+  }
 
-    if (!userData) {
-        return null;
-      }    
+  console.log(userData);
+  const { nombre, apellido, email } = userData;
+  return (
+    <div>
+      <div className="">
+        <h2>Información del usuario:</h2>
+        <label>Nombre:</label> <input type="text" value={nombre} />
+        <p>
+          <label>Apellido:</label> <input type="text" value={apellido} />
+        </p>
+        <p>
+          <label>Email:</label> <input type="text" value={email} />
+        </p>
+      </div>
+    </div>
+  );
+};
 
-    console.log(userData);
-    const { nombre, apellido, email } = userData;
-    return (
-        <div>
-            <div className="">
-                <h2>Información del usuario:</h2>
-                <label >Nombre:</label> <input type="text" value={nombre} />
-                <p><label >Apellido:</label> <input type="text" value={apellido} /></p>
-                <p><label >Email:</label> <input type="text" value={email} /></p>
-            </div>
-        </div>
-    )
-}
-
-export default UserPage
+export default UserPage;
