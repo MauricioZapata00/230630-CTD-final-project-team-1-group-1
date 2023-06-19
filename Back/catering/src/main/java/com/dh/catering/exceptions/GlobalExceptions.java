19c9ccb974e8be3379cc8040db87cce3495baaab
Giuliana Borgo <giulianaborgo4@gmail.com>
2023-06-19 14:44:43 -0300

    cambio mensaje registro

--- Back/catering/src/main/java/com/dh/catering/exceptions/GlobalExceptions.java
+++ Back/catering/src/main/java/com/dh/catering/exceptions/GlobalExceptions.java
@@ -68,4 +68,14 @@ public class GlobalExceptions {
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(mensajeError);
     }
+
+    @ExceptionHandler({DuplicadoException.class})
+    public ResponseEntity<MensajeError> procesarDuplicadoException(DuplicadoException e){
+        MensajeError mensajeError = new MensajeError();
+        mensajeError.setMessage("Lo sentimos, ha ocurrido un error!");
+        mensajeError.setDescription(e.getMessage());
+        mensajeError.setStatusCode(1007);
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(mensajeError);
+    }
 }
