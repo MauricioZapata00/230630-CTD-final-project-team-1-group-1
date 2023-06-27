12ffe6a36371df832ed257cad8d9e64e2e2421c3
Jessica J <jessica.mjs@hotmail.com>
2023-06-27 03:59:16 +0000

    Cobertura completa CategoriaProductoService

--- Back/catering/src/test/java/com/dh/catering/service/CategoriaProductoServiceTest.java
+++ Back/catering/src/test/java/com/dh/catering/service/CategoriaProductoServiceTest.java
@@ -1,6 +1,10 @@
 package com.dh.catering.service;
 
+import com.dh.catering.domain.Producto;
+import com.dh.catering.dto.CategoriaProductoDto;
+import com.dh.catering.exceptions.AsignacionException;
 import com.dh.catering.exceptions.NombreDuplicadoException;
+import com.dh.catering.exceptions.RecursoNoEncontradoException;
 import com.dh.catering.factory.CategoriaProductoFactory;
 import com.dh.catering.repository.CategoriaProductoRepository;
 import com.dh.catering.repository.ProductoRepository;
@@ -13,6 +17,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.mockito.ArgumentMatchers.any;
@@ -23,6 +28,8 @@ import static org.mockito.Mockito.*;
 class CategoriaProductoServiceTest {
 
     private static final ObjectMapper MAPPER = new ObjectMapper();
+    public static final String TEST = "test";
+    public static final long ID = 1L;
     @Mock
     private CategoriaProductoRepository categoriaProductoRepository;
     @Mock
@@ -33,8 +40,8 @@ class CategoriaProductoServiceTest {
 
     @BeforeEach
     void setUp() {
-        service = new CategoriaProductoService(categoriaProductoRepository,
-                productoRepository, MAPPER, s3ImageService);
+        service = spy(new CategoriaProductoService(categoriaProductoRepository,
+                productoRepository, MAPPER, s3ImageService));
     }
 
     @Test
@@ -60,5 +67,79 @@ class CategoriaProductoServiceTest {
                 .when(categoriaProductoRepository).save(any());
         Assertions.assertTrue(service.save(CategoriaProductoFactory.iniciarCategoriaProductoDto(), file).isPresent());
     }
-}
 
+    @Test
+    void testFindAll() {
+        when(categoriaProductoRepository.findAll())
+                .thenReturn(List.of(CategoriaProductoFactory.iniciarCategoriaProducto()));
+        var resultado = service.findAll();
+        Assertions.assertNotNull(resultado);
+        Assertions.assertFalse(resultado.isEmpty());
+        Assertions.assertTrue(resultado.get(0) instanceof CategoriaProductoDto);
+    }
+
+    @Test
+    void testGetByIdLanzaExcepcion() {
+        when(categoriaProductoRepository.findById(any()))
+                .thenReturn(Optional.empty());
+        Assertions.assertThrows(RecursoNoEncontradoException.class,
+                () -> service.getById(ID));
+    }
+
+    @Test
+    void testGetById() throws RecursoNoEncontradoException {
+        when(categoriaProductoRepository.findById(any()))
+                .thenReturn(Optional.of(CategoriaProductoFactory.iniciarCategoriaProducto()));
+        Assertions.assertTrue(service.getById(ID).isPresent());
+    }
+
+    @Test
+    void testGetByNombreLanzaExcepcion() {
+        when(categoriaProductoRepository.getByNombre(any()))
+                .thenReturn(Optional.empty());
+        Assertions.assertThrows(RecursoNoEncontradoException.class,
+                () -> service.getByNombre(TEST));
+    }
+
+    @Test
+    void testGetByNombre() throws RecursoNoEncontradoException {
+        when(categoriaProductoRepository.getByNombre(any()))
+                .thenReturn(Optional.of(CategoriaProductoFactory.iniciarCategoriaProducto()));
+        Assertions.assertTrue(service.getByNombre(TEST).isPresent());
+    }
+
+    @Test
+    void testDeleteByIdLanzaExcepcion() throws RecursoNoEncontradoException {
+        doReturn(Optional.of(CategoriaProductoFactory.iniciarCategoriaProductoDto()))
+                .when(service).getById(anyLong());
+        when(productoRepository.findAllByCategoriaId(any()))
+                .thenReturn(List.of(new Producto()));
+        Assertions.assertThrows(AsignacionException.class,
+                () -> service.deleteById(ID));
+    }
+
+    @Test
+    void testDeleteByIdNull() throws RecursoNoEncontradoException, AsignacionException {
+        doReturn(Optional.empty()).when(service).getById(anyLong());
+        Assertions.assertTrue(service.deleteById(ID).isEmpty());
+    }
+
+    @Test
+    void testDeleteById() throws RecursoNoEncontradoException, AsignacionException {
+        doReturn(Optional.of(CategoriaProductoFactory.iniciarCategoriaProductoDto()))
+                .when(service).getById(anyLong());
+        when(productoRepository.findAllByCategoriaId(any()))
+                .thenReturn(List.of());
+        doNothing().when(categoriaProductoRepository).deleteById(any());
+        Assertions.assertTrue(service.deleteById(ID).isPresent());
+    }
+
+    @Test
+    void updateById(@Mock MultipartFile file) throws RecursoNoEncontradoException, AsignacionException, NombreDuplicadoException {
+        doReturn(Optional.empty()).when(service).deleteById(any());
+        doReturn(Optional.empty()).when(service).save(any(), any());
+        doReturn(Optional.of(CategoriaProductoFactory.iniciarCategoriaProductoDto()))
+                .when(service).getByNombre(anyString());
+        Assertions.assertTrue(service.updateById(ID, CategoriaProductoFactory.iniciarCategoriaProductoDto(), file).isPresent());
+    }
+}
