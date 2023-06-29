8ec94e76f6e6293135df8ca7f2ccc6a2f1562472
Carlos Castellar <carloscastellarsanchez@gmail.com>
2023-06-28 22:18:16 -0500

    se crea el envio de correo de confirmacion de reserva

--- Back/catering/src/main/java/com/dh/catering/service/ReservaService.java
+++ Back/catering/src/main/java/com/dh/catering/service/ReservaService.java
@@ -32,6 +32,9 @@ public class ReservaService {
     @Autowired
     private UsuarioRepository usuarioRepository;
 
+    @Autowired
+    private EmailService emailService;
+
     public Optional<String> registrar(ReservaDto reservaDto) throws AsignacionException {
         String mensaje = null;
         if (reservaDto != null){
@@ -45,6 +48,7 @@ public class ReservaService {
             Usuario usuario = usuarioRepository.findByEmail(reservaDto.getEmailUsuario()).get();
             Reserva reserva = new Reserva(Util.convertirStringToLocalDate(reservaDto.getFechaReserva()),producto,usuario);
             reservaRepository.save(reserva);
+            emailService.send(reservaDto.getEmailUsuario(),confirmacionReserva(usuario.getNombre(), producto.getNombre(), producto.getId(), reservaDto.getFechaReserva()));
             mensaje = "Ha realizado su reserva con exito!";
             log.info(mensaje);
         }
@@ -122,5 +126,61 @@ public class ReservaService {
         log.info(mensaje);
         return Optional.ofNullable(mensaje);
     }
+
+    private String confirmacionReserva(String nombreUsuario, String nombreProducto, Long idProducto, String fechaReserva) {
+        String emailContent = "<div style=\"font-family:Helvetica,Arial,sans-serif;font-size:16px;margin:0;color:#0b0c0c\">\n" +
+                "\n" +
+                "<span style=\"display:none;font-size:1px;color:#fff;max-height:0\"></span>\n" +
+                "\n" +
+                "  <table role=\"presentation\" width=\"100%\" style=\"border-collapse:collapse;min-width:100%;width:100%!important\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\">\n" +
+                "    <tbody><tr>\n" +
+                "      <td width=\"100%\" height=\"53\" bgcolor=\"#0b0c0c\">\n" +
+                "        \n" +
+                "        <table role=\"presentation\" width=\"100%\" style=\"border-collapse:collapse;max-width:580px\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\" align=\"center\">\n" +
+                "          <tbody><tr>\n" +
+                "            <td width=\"70\" bgcolor=\"#0b0c0c\" valign=\"middle\">\n" +
+                "                <table role=\"presentation\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\" style=\"border-collapse:collapse\">\n" +
+                "                  <tbody><tr>\n" +
+                "                    <td style=\"padding-left:10px\">\n" +
+                "                  \n" +
+                "                    </td>\n" +
+                "                    <td style=\"font-size:28px;line-height:1.315789474;Margin-top:4px;padding-left:10px\">\n" +
+                "                      <span style=\"font-family:Helvetica,Arial,sans-serif;font-weight:700;color:#ffffff;text-decoration:none;vertical-align:top;display:inline-block\">Confirmación de reserva</span>\n" +
+                "                    </td>\n" +
+                "                  </tr>\n" +
+                "                </tbody></table>\n" +
+                "              </a>\n" +
+                "            </td>\n" +
+                "          </tr>\n" +
+                "        </tbody></table>\n" +
+                "        \n" +
+                "      </td>\n" +
+                "    </tr>\n" +
+                "  </tbody></table>\n" +
+                "  <table role=\"presentation\" class=\"m_-6186904992287805515content\" align=\"center\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\" style=\"border-collapse:collapse;max-width:580px;width:100%!important\" width=\"100%\">\n" +
+                "    <tbody><tr>\n" +
+                "      <td height=\"30\"><br></td>\n" +
+                "    </tr>\n" +
+                "    <tr>\n" +
+                "      <td width=\"10\" valign=\"middle\"><br></td>\n" +
+                "      <td style=\"font-family:Helvetica,Arial,sans-serif;font-size:19px;line-height:1.315789474;max-width:560px\">\n" +
+                "        \n" +
+                "            <p style=\"Margin:0 0 20px 0;font-size:19px;line-height:25px;color:#0b0c0c\">¡Hola " + nombreUsuario + ",</p>\n" +
+                "            <p style=\"Margin:0 0 20px 0;font-size:19px;line-height:25px;color:#0b0c0c\">Has realizado la reserva del producto '" + nombreProducto + "' con ID " + idProducto + " para el día " + fechaReserva + ".</p>\n" +
+                "            <p style=\"Margin:0 0 20px 0;font-size:19px;line-height:25px;color:#0b0c0c\">¡Disfruta de tu reserva!</p>\n" +
+                "        \n" +
+                "      </td>\n" +
+                "      <td width=\"10\" valign=\"middle\"><br></td>\n" +
+                "    </tr>\n" +
+                "    <tr>\n" +
+                "      <td height=\"30\"><br></td>\n" +
+                "    </tr>\n" +
+                "  </tbody></table><div class=\"yj6qo\"></div><div class=\"adL\">\n" +
+                "\n" +
+                "</div></div>";
+
+        return emailContent;
+    }
+
 
 }
